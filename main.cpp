b12c28dd124dcfe5e3f52e2123ddfabfbd1dc9fc
gameplay2050 <brainfastmans2050@gmail.com>
2021-09-21 18:28:08 +0300

    Updated Source Cpp Source File

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+main()
+{
+	
+	int fibonacci,numOne=0,numTwo=1,Result,j;
+	cout<<"Select number of fibonacci series view : ";
+	cin>>fibonacci;
+		for(j=0;j<=fibonacci;j++)
+		{
+			
+			Result = numOne + numTwo;
+			cout<<" "<<Result;
+			numOne = numTwo;
+			numTwo = Result; 
+			        	    	      
+		}
+       
+       return 0;
+}
